d0ed8167f268c11496b44f0b54225774589a45cd
Pratik Sah <shivam3448@gmail.com>
2020-07-08 21:52:58 +0530

    Added old school for loop

--- ForOfLoop.js
+++ ForOfLoop.js
@@ -5,3 +5,13 @@ const array = [1, 2, 3];
 for (const i of array) {
   console.log(i);
 }
+
+// Old school for loops
+
+const i = 1;
+
+// to print from 1 to 20
+
+for (i = 1; i <= 20; i++) {
+  console.log(i);
+}
