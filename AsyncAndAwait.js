574794e322e14cf7723d5a2d70c6424555ef251c
Pratik Sah <shivam3448@gmail.com>
2020-10-04 22:01:07 +0530

    Fetched data from github api and rendered in html

--- AsyncAndAwait.js
+++ AsyncAndAwait.js
@@ -1,12 +1,12 @@
-function getUser(name) {
-  fetch(`https://api.github.com/users/${name}`)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (json) {
-      console.log(json);
-    });
-}
+// function getUser(name) {
+//   fetch(`https://api.github.com/users/${name}`)
+//     .then(function (response) {
+//       return response.json();
+//     })
+//     .then(function (json) {
+//       console.log(json);
+//     });
+// }
 
 async function getUser(name) {
   const res = await fetch(`https://api.github.com/users/${name}`);
@@ -16,5 +16,20 @@ async function getUser(name) {
 
 //get user data
 getUser("ThePratikSah").then((data) => {
-  console.log(data.name);
+  // setting data from the github api
+  let img = document.querySelector("#card-img");
+  let p = document.querySelector("#card-title");
+  let bio = document.querySelector(".bio");
+  let url = document.querySelector("#github-url");
+
+  img.src = data.avatar_url;
+  p.innerHTML = data.name;
+  bio.innerHTML = data.bio;
+  url.href = data.html_url;
+
+  document.querySelector(".loader").style.display = "none";
+  document.querySelector(".card").style.display = "block";
+
 });
+
+
